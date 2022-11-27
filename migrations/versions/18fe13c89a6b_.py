897e293c373e690a1bd48588cf16d64a9104cb4a
Ptruongg <105082177+Ptruongg@users.noreply.github.com>
2022-11-27 13:35:10 -0800

    Update 18fe13c89a6b_.py

--- migrations/versions/18fe13c89a6b_.py
+++ migrations/versions/18fe13c89a6b_.py
@@ -8,6 +8,9 @@ Create Date: 2022-11-27 13:33:43.819745
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '18fe13c89a6b'
@@ -54,6 +57,8 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+    if environment == "production":
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
