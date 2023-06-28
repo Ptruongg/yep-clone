5314e7cb36b352dc5bd473491854c296831e480f
Philip <philiptwith1l@gmail.com>
2023-06-28 13:40:26 -0700

    fix

--- app/s3_helpers.py
+++ app/s3_helpers.py
@@ -1,4 +1,4 @@
-import boto3
+# import boto3
 import botocore
 import os
 import uuid
@@ -7,11 +7,11 @@ BUCKET_NAME = os.environ.get("S3_BUCKET")
 S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
 ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
 
-s3 = boto3.client(
-   "s3",
-   aws_access_key_id=os.environ.get("S3_KEY"),
-   aws_secret_access_key=os.environ.get("S3_SECRET")
-)
+# s3 = boto3.client(
+#    "s3",
+#    aws_access_key_id=os.environ.get("S3_KEY"),
+#    aws_secret_access_key=os.environ.get("S3_SECRET")
+# )
 
 # print("aws_access_key_id", os.environ.get("S3_KEY"), os.environ.get("S3_SECRET"))
 
@@ -31,20 +31,20 @@ def get_unique_filename(filename):
 
 
 
-def upload_file_to_s3(file, acl="public-read"):
-    print("filefilefilefilefilefile", file)
-    try:
-        s3.upload_fileobj(
-            file,
-            BUCKET_NAME,
-            file.filename,
-            ExtraArgs={
-                "ACL": acl,
-                "ContentType": file.content_type
-            }
-        )
-    except Exception as e:
-        # in case the our s3 upload fails
-        return {"errors": str(e)}
+# def upload_file_to_s3(file, acl="public-read"):
+#     print("filefilefilefilefilefile", file)
+#     try:
+#         s3.upload_fileobj(
+#             file,
+#             BUCKET_NAME,
+#             file.filename,
+#             ExtraArgs={
+#                 "ACL": acl,
+#                 "ContentType": file.content_type
+#             }
+#         )
+#     except Exception as e:
+#         # in case the our s3 upload fails
+#         return {"errors": str(e)}
 
-    return {"url": f"{S3_LOCATION}{file.filename}"}
+#     return {"url": f"{S3_LOCATION}{file.filename}"}
