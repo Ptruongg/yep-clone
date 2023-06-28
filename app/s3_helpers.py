511651541fa1f7012a48a67e8653440f350de35a
Philip <philiptwith1l@gmail.com>
2023-06-28 13:48:23 -0700

    commented out

--- app/s3_helpers.py
+++ app/s3_helpers.py
@@ -1,11 +1,11 @@
 # import boto3
-import botocore
-import os
-import uuid
+# import botocore
+# import os
+# import uuid
 
-BUCKET_NAME = os.environ.get("S3_BUCKET")
-S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
-ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
+# BUCKET_NAME = os.environ.get("S3_BUCKET")
+# S3_LOCATION = f"https://{BUCKET_NAME}.s3.amazonaws.com/"
+# ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
 
 # s3 = boto3.client(
 #    "s3",
@@ -18,16 +18,16 @@ ALLOWED_EXTENSIONS = {"pdf", "png", "jpg", "jpeg", "gif"}
 # print("s33333333333333333333@@@@@@@@@@@@", s3)
 
 
-def allowed_file(filename):
-    return "." in filename and \
-           filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+# def allowed_file(filename):
+#     return "." in filename and \
+#            filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 
-def get_unique_filename(filename):
-    ext = filename.rsplit(".", 1)[1].lower()
-    unique_filename = uuid.uuid4().hex
-    return f"{unique_filename}.{ext}"
+# def get_unique_filename(filename):
+#     ext = filename.rsplit(".", 1)[1].lower()
+#     unique_filename = uuid.uuid4().hex
+#     return f"{unique_filename}.{ext}"
 
 
 
